bcd4fd5c28ce710f9d87494321d9514bf41650cc
Yurina Shin <yurinas@gmail.com>
2018-10-01 20:48:24 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,7 +2,7 @@ class Helpers
 
   def self.current_user(session)
     binding.pry
-    User.find_by_id(@user.id)
+    User.find_by(:id=>session[:user_id])
   end
 
   def self.is_logged_in?(session)
